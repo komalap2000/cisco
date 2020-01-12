f473f645d285fd1260d52741a9b7ef714c09a80b
Komal Patel <kap52644@uga.edu>
2020-01-12 13:36:23 -0500

    this will test the 2 methogs

---
+++ src/main/java/cisco/internship/Driver.java
@@ -0,0 +1,29 @@
+package cisco.internship;
+
+import java.util.ArrayList;
+
+public class Driver {
+
+    public static void main(String[] args) {
+	// using the nodes provided in the graph
+	Graph A = new Graph("A");
+	Graph B = new Graph("B");
+	Graph C	= new Graph("C");
+	Graph D	= new Graph("D");
+	Graph E	= new Graph("E");
+	Graph F	= new Graph("F");
+	Graph G	= new Graph("G");
+	Graph H	= new Graph("H");
+	Graph I	= new Graph("I");
+	Graph J	= new Graph("J");
+
+	/*
+	GNode[] aChildren = new GNode[] {};
+	GNode[] bChildren = new GNode[] {};
+	GNode[] cChildren = new GNode[] {};
+	GNode[] dChildren = new GNode[] {};
+	*/
+	System.out.println("Solutions to problems 1 and 2 are provided below");
+	
+    }
+}
