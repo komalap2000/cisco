7e48863b9cb4088fe7297110590c3b0ce6d6b2ea
Komal Patel <kap52644@uga.edu>
2020-01-13 00:09:21 -0500

    added a driver

--- src/main/java/cisco/internship/Driver.java
+++ src/main/java/cisco/internship/Driver.java
@@ -2,28 +2,47 @@ package cisco.internship;
 
 import java.util.ArrayList;
 
+/**
+ * This will be the tester class.
+ */
 public class Driver {
 
+    /**
+     * This is the main method where it will be tested.
+     */
     public static void main(String[] args) {
-	// using the nodes provided in the graph
-	Graph A = new Graph("A");
-	Graph B = new Graph("B");
-	Graph C	= new Graph("C");
-	Graph D	= new Graph("D");
-	Graph E	= new Graph("E");
-	Graph F	= new Graph("F");
-	Graph G	= new Graph("G");
-	Graph H	= new Graph("H");
-	Graph I	= new Graph("I");
-	Graph J	= new Graph("J");
+        // using the nodes provided in the graph
+        Graph A = new Graph("A");
+        Graph B = new Graph("B");
+        Graph C = new Graph("C");
+        Graph D = new Graph("D");
+        Graph E = new Graph("E");
+        Graph F = new Graph("F");
+        Graph G = new Graph("G");
+        Graph H = new Graph("H");
+        Graph I = new Graph("I");
+        Graph J = new Graph("J");
 
-	/*
-	GNode[] aChildren = new GNode[] {};
-	GNode[] bChildren = new GNode[] {};
-	GNode[] cChildren = new GNode[] {};
-	GNode[] dChildren = new GNode[] {};
-	*/
-	System.out.println("Solutions to problems 1 and 2 are provided below");
+	GNode[] aChildren = new GNode[] {B, C, D};
+	A.setChild(aChildren);
+
+	GNode[] bChildren = new GNode[] {E, F};
+	B.setChild(bChildren);
 	
+	GNode[] cChildren = new GNode[] {G, H, I};
+	C.setChild(cChildren);
+	
+	GNode[] dChildren = new GNode[] {J};
+        D.setChild(dChildren);
+	
+        System.out.println("Solutions to problems 1 and 2 are provided below");
+
+	// Problem 1
+	ArrayList<GNode> finalNodes = A.walkGraph(A);
+	System.out.println("The ArrayList that contains each node in the graph are: " + finalNodes);
+
+	// Problem 2
+	 ArrayList<ArrayList<GNode>> arrayPaths = A.paths(A);
+	 System.out.println("The ArrayList of the possible paths are: " + arrayPaths);
     }
 }
