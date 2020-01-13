6612dcb273d5cb7bc5583de0ef32d4c0def0a43b
Komal Patel <kap52644@uga.edu>
2020-01-13 00:08:18 -0500

    fixed checkstyle

--- src/main/java/cisco/internship/GNode.java
+++ src/main/java/cisco/internship/GNode.java
@@ -1,17 +1,20 @@
 package cisco.internship;
 
+/**
+ * This is an interface that will be used in other classes.
+ */
 public interface GNode {
 
     /**
-     * This the name of the graph node (GNode)
+     * This the name of the graph node (GNode).
+     * @return a name of type String
      */
     String getName();
 
     /**
      * This function returns an array of children GNodes. When a GNode has no children,
      * it returns an array of size 0.
-     *
-     * @return
+     * @return an array of type GNode
      */
     GNode[] getChildren();
 }
